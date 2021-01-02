20df413db8a6f9a7e2616e337a86e3124f97a59d
TechnoFrost27 <60550481+TechnoFrost27@users.noreply.github.com>
2021-01-02 21:40:08 +0530

    Squarey Version 1
    This is the first version, playable, what we made at our house

---
+++ Android Game/Assets/Scripts/Collision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour{
+     public PlayerControls controls;
+     public GameObject objectToDisable;
+     public static bool disabled =false;
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            controls.enabled=false;
+            objectToDisable.active = true;
+            
+        }
+
+                if (collision.gameObject.tag == "Lava")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            controls.enabled=false;
+        }
+  
+        
+    }
+
+}
