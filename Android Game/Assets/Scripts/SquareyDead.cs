a4c0820409f5afe6b979657c334d2ff201275537
TechnoFrost27 <60550481+TechnoFrost27@users.noreply.github.com>
2021-01-04 13:04:39 +0530

    So many changes!!
    Added fire eyes that show when you fall in lava, added text that says squarey was bbqed. Also added lots of cool fonts so the UI looks better. Created more font assests that we need so can use in future.

--- Android Game/Assets/Scripts/SquareyDead.cs
+++ Android Game/Assets/Scripts/SquareyDead.cs
@@ -9,13 +9,18 @@ public class SquareyDead : MonoBehaviour{
 
         public PlayerControls controls;
         //makes a reference to the playercontorls function which we can enable or disable
-        public GameObject DieText;
+        public GameObject DieText;  
+        public GameObject LavaText;
+
         //adds reference to the gameobject which we can activate
         public GameObject RestartButton;
         public GameObject MenuButton;
         public GameObject StarEyes1;
         public GameObject StarEyes2;
         public GameObject Eyes;
+        public GameObject Fireeye1;
+        public GameObject Fireeye2;
+        public Camera cam;
 
         public static bool enabled =false;
                private void OnCollisionEnter2D(Collision2D collision)//function for collision
@@ -32,5 +37,16 @@ public class SquareyDead : MonoBehaviour{
                   Eyes.active = false;
 
                 }
+                   if (collision.gameObject.tag == "Lava")
+        {
+            controls.enabled=false;
+             Fireeye1.active = true;
+             Fireeye2.active = true;
+             Eyes.active = false;
+             cam.orthographicSize = 3.0f;
+             RestartButton.active = true;
+             MenuButton.active = true;       
+             LavaText.active = true;     
+        }//disables controls in Lava
                 }  
                         }
---
+++ Android Game/Assets/Scripts/follow_camera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class follow_camera : MonoBehaviour{
+	public Transform playerboii;
+	public Vector3 offset;
+    // Update is called once per frame
+    void Update()
+    {
+       transform.position=playerboii.position + offset;
+
+    }
+    
+}
+ 
